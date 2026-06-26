74815e5da8191a5e2e80b20c4cafc4b93d222f95
Jay <jayrickaby@pm.me>
2026-06-26 14:18:19 +0100

    feat: add experimentally query

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ import musicbrainzngs
 
 def initialise():
     # Load before others to properly initialise env variables
+    print("Initialising environment variables...")
     load_dotenv()
     musicBrainzUsername = os.getenv("MUSICBRAINZ_USERNAME")
     musicBrainzPassword = os.getenv("MUSICBRAINZ_PASSWORD")
 
+    print("Setting MusicBrainz useragent...")
     musicbrainzngs.set_useragent("proof of concept application", "0.0.0", contact="jayrickaby@pm.me")
+    print("Authorising MusicBrainz credentials...")
     musicbrainzngs.auth(musicBrainzUsername, musicBrainzPassword)
 
 def main():
     initialise()
+    test = musicbrainzngs.search_artists(query="King Crimson")
+    print(test['artist-list'][0]['id'])
+    print(test['artist-list'][0]['name'])
 
 if __name__ == '__main__':
     main()

cdcd975ec2db361512af7e50874f39d74b72449a
Jay <jayrickaby@pm.me>
2026-06-26 14:12:43 +0100

    feat: add musicbrainz api functionality

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from dotenv import load_dotenv
+import os
+
+# For this proof-of-concept, we will use a few third party libraries in order to perform API requests.
+# For the actual programming project, API requests will all be done in-house.
+import musicbrainzngs
+
+def initialise():
+    # Load before others to properly initialise env variables
+    load_dotenv()
+    musicBrainzUsername = os.getenv("MUSICBRAINZ_USERNAME")
+    musicBrainzPassword = os.getenv("MUSICBRAINZ_PASSWORD")
+
+    musicbrainzngs.set_useragent("proof of concept application", "0.0.0", contact="jayrickaby@pm.me")
+    musicbrainzngs.auth(musicBrainzUsername, musicBrainzPassword)
+
 def main():
-    print("u u wa wa uwa")
+    initialise()
 
 if __name__ == '__main__':
     main()

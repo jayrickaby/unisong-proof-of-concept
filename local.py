90d2545d83b27f6aaba3dcbcc2b345de52e8073c
Jay Rickaby <jayrickaby@pm.me>
2026-06-27 22:51:04 +0100

    feat: prefer root_dir

--- local.py
+++ local.py
@@ -6,6 +6,6 @@ EXTENSIONS = ["*.flac", "*.wav", "*.mp3"]
 def searchForAlbums(path):
     files = []
     for extension in EXTENSIONS:
-        files.extend(glob.glob(f"{path}/{extension}"))
+        files.extend(glob.glob(extension, root_dir=path))
 
     print(files)

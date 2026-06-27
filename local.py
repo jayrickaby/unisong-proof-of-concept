3d1af116f39e98ae789cd18eefef2b1181b3860d
Jay Rickaby <jayrickaby@pm.me>
2026-06-27 22:50:39 +0100

    feat: prefer extend over append to ignore empty returns

--- local.py
+++ local.py
@@ -6,6 +6,6 @@ EXTENSIONS = ["*.flac", "*.wav", "*.mp3"]
 def searchForAlbums(path):
     files = []
     for extension in EXTENSIONS:
-        files.append(glob.glob(f"{path}/{extension}"))
+        files.extend(glob.glob(f"{path}/{extension}"))
 
     print(files)

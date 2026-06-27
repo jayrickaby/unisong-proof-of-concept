aa03e006fa01ac6a78660095e2dee0e52cfd20a8
Jay Rickaby <jayrickaby@pm.me>
2026-06-27 22:37:24 +0100

    refactor: remove duplicate code from main.py

--- main.py
+++ main.py
@@ -12,34 +12,6 @@ def initialise():
 def searchAlbum(query, limit):
     return musicbrainzngs.search_release_groups(query=query, limit=limit, type='album')
 
-def createAlbumTable():
-    db = sqlite3.connect("unisong.db")
-    cursor = db.cursor()
-    q = """
-        CREATE TABLE IF NOT EXISTS albums (
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL,
-            artist TEXT NOT NULL,
-            year INT NOT NULL,
-            description STRING NOT NULL
-            ); 
-        """
-    cursor.execute(q)
-    cursor.close()
-
-def addAlbum(newAlbum):
-    db = sqlite3.connect("unisong.db")
-    cursor = db.cursor()
-
-    q = f"""
-        INSERT INTO albums (title, artist, year, description) VALUES (?, ?, ?, ?)
-    """
-
-    cursor.execute(q, (newAlbum.title, newAlbum.artists[0], 0, "the best"))
-    db.commit()
-    cursor.close()
-
-
 def main():
     initialise()
 

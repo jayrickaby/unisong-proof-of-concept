08c2b10ac0aa56960b709a71cd50d0ecb65317be
Jay Rickaby <jayrickaby@pm.me>
2026-06-28 20:08:47 +0100

    feat: adjust unisong album table

--- db.py
+++ db.py
@@ -3,11 +3,12 @@ import sqlite3
 CENTRAL_DB = "unisong.db"
 # LOCAL_DB = "local.db" ... for later
 
-def createAlbumTable():
+def createUnisongAlbumTable():
     db = sqlite3.connect(CENTRAL_DB)
     q = """
         CREATE TABLE IF NOT EXISTS albums (
             id INTEGER PRIMARY KEY,
+            releaseGroupMBID TEXT NOT NULL,
             title TEXT NOT NULL,
             artist TEXT NOT NULL,
             year INTEGER NOT NULL,
@@ -17,16 +18,16 @@ def createAlbumTable():
     db.execute(q)
     db.close()
 
+def
+
 def addAlbum(newAlbum):
     db = sqlite3.connect(CENTRAL_DB)
     cursor = db.cursor()
 
     q = """
-        INSERT INTO albums (title, artist, year, description) VALUES (?, ?, ?, ?);
+        INSERT INTO albums (releaseGroupMBID, title, artist, year, description) VALUES (?, ?, ?, ?, ?);
     """
 
-    cursor.execute(q,
-                   (newAlbum.title, newAlbum.artists[0], 0, "")
-                   )
+    cursor.execute(q,(newAlbum.releaseGroupMBID, newAlbum.title, newAlbum.artists[0], newAlbum.year, ""))
     db.commit()
     cursor.close()

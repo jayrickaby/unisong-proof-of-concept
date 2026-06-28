c661c7486193ca58839a9d58c19f0f115963142e
Jay Rickaby <jayrickaby@pm.me>
2026-06-28 20:29:50 +0100

    feat: add local track

---
+++ poc/LocalTrack.py
@@ -0,0 +1,25 @@
+class LocalTrack:
+    def __init__(self):
+        # Required db fields
+        self.trackMBID = ""
+        self.path = ""
+
+        # Extra stuff to help album / release cataloging
+        self.title = ""
+        self.artists = []
+        self.year = ""
+        self.release = ""
+
+    def parseLocalTrack(self, data):
+        # use .get() as it returns 'None' if invalid
+        if data.get("title"):
+            self.title = data["title"][0]
+
+        if data.get("album"):
+            self.release = data["album"][0]
+
+        if data.get("artist"):
+            self.artists = data["artist"][0]
+
+        if data.get("year"):
+            self.year = data["year"][0]

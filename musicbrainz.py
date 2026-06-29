69a06b1d3d2c51d3da3e7bd3d3fe14b420257e57
Jay <jayrickaby@pm.me>
2026-06-29 13:27:11 +0100

    feat: change musicbrainz code to getReleaseIDs from Local Releases

--- musicbrainz.py
+++ musicbrainz.py
@@ -1,27 +1,21 @@
 import musicbrainzngs
+from poc.LocalRelease import LocalRelease
 
 DEFAULT_LIMIT = 1
 
-def searchAlbum(query, limit=DEFAULT_LIMIT):
-    return musicbrainzngs.search_release_groups(query=query, limit=limit, type='album')
-
-def getIdFromAlbum(album):
-    # Assume all albums have a title, but may have a disambiguation
-    pureTitle = album.title.split("(")[0]
-    print("Looking for album with pure title: ", pureTitle)
-
-    result = {}
-
-    if album.artists:
-        result = musicbrainzngs.search_release_groups(query=pureTitle, artistname=album.artists[0] ,limit=DEFAULT_LIMIT, type='album')
-
+def getReleaseIdFromLocalRelease(release: LocalRelease):
+    # TODO: Surely theres a better way to do this?
+    if len(release.artists) == 0 or release.year is None:
+        result = musicbrainzngs.search_releases(
+            query=release.title,
+            comment=release.disambiguation,
+            limit=DEFAULT_LIMIT, type='album')
     else:
-        result = musicbrainzngs.search_release_groups(query=pureTitle, limit=DEFAULT_LIMIT, type='album')
+        result = musicbrainzngs.search_releases(
+            query=release.title,
+            comment=release.disambiguation,
+            artistname=release.artists[0],
+            date=release.year,
+            limit=DEFAULT_LIMIT, type='album')
 
-    data = result['release-group-list']
-
-    if len(data) > 0:
-        print("Found with id: " + data[0]['id'])
-        return data[0]['id']
-
-    return None
+    return result['release-list'][0]['id']
